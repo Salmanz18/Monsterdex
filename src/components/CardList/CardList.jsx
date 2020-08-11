0669ee04a4f71f74acedde9a3c9435a88f04aba0
Salman Zafar <salman.x916@gmail.com>
2020-08-12 02:26:56 +0530

    Initial Commit

---
+++ src/components/CardList/CardList.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import './CardList.css'
+
+export const CardList = (props) => {
+  console.log(props)
+  return (
+    <div className="card-list">
+      {props.children}
+    </div>
+  )
+}
