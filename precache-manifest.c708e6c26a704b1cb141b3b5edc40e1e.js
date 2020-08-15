815c9b8f963606ed57b8719f42de0cda3944ee59
Salman Zafar <salman.x916@gmail.com>
2020-08-16 02:37:50 +0530

    Updates

---
+++ precache-manifest.c708e6c26a704b1cb141b3b5edc40e1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aef470cd8ffe70fefdf1ad9b1419040c",
+    "url": "/Monsterdex/index.html"
+  },
+  {
+    "revision": "d4bf5608f808c7c73720",
+    "url": "/Monsterdex/static/css/main.cf00e527.chunk.css"
+  },
+  {
+    "revision": "0414063dac2cacd9051a",
+    "url": "/Monsterdex/static/js/2.ff5662b8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsterdex/static/js/2.ff5662b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4bf5608f808c7c73720",
+    "url": "/Monsterdex/static/js/main.8eb6ea51.chunk.js"
+  },
+  {
+    "revision": "c2a906a19ba919b7138f",
+    "url": "/Monsterdex/static/js/runtime-main.4b47b852.js"
+  }
+]);
